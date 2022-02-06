d2849cf3d41af48cd3577a7c7c16ff9c758498db
yangddu <wndun21@naver.com>
2022-02-06 13:32:36 +0900

    Add: action type 추가

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  console.log(count);
-  return count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 //reducer is a function that modifies your data!
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
